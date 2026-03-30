56811613ed8625bffccc3e96415ec23b9449fb4e
Mariana <marianamalagutti@yahoo.com>
2026-03-30 09:15:45 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
-alert("Hello World !!!")
+//Declarações e variáveis
+//var, let e const
+
+var nome="Fiap";
+console.log(nome)
+
+var nome="Teste";
+console.log(nome)
+
+if(true){
+    let apelido="Fiapinho";
+    console.log(apelido)
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros)
+//let taxaJuros=1.5
+
+//typeof-verifica o tipo da variavel
+var exemplo="ola-dev-var";
+console.log(exemplo)
+
+let exemplo1="ola-dev-let";
+console.log(exemplo1)
+
+const exemplo2="ola-dev-const";
+console.log(exemplo2)
+
+let exemplo3=10;
+console.log(exemplo3)
+
+let exemplo4=true;
+console.log(exemplo4)
+
+let exemplo5={nome1:"teste"}
+console.log(exemplo5)
+
+let exemplo6=["Java", "c#", "PHP"];
+console.log(exemplo6)
+
+//valor indefinido
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8 =null
+console.log(exemplo8)
